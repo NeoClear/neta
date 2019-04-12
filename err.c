6d964b6821c7b0c89e84e4ab8bf5fefccb12cd26
NeoClear <neoclear@outlook.com>
2019-04-12 15:06:44 +0800

    small fix

--- err.c
+++ err.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Print standard error
 void err(char *s)
 {
     fprintf(stderr, "stderr: %s\n", s);
     exit(0);
 }
 
+// Print parse error
 void parse_err(char *expect, char *encounter)
 {
     fprintf(stderr, "parsing error: expected %s, encountered %s\n", expect, encounter);
     exit(0);
 }
 
+// Print runtime error
 void runtime_err(char *expect, char *encounter)
 {
     fprintf(stderr, "runtime error: expected %s, encountered %s\n", expect, encounter);
