186f9f26d6666bacacbe8e561a01dfbe86e67d05
NeoClear <neoclear@outlook.com>
2020-04-04 00:28:50 -0400

    able to use repl directly

--- app.c
+++ app.c
@@ -12,13 +12,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include "err.h"
+#include <string.h>
 
 extern FILE *yyin;
 extern int yyleng;
 
 int yylex();
-
-int shit();
 
 void init()
 {
@@ -27,10 +26,32 @@ void init()
 
 int main(int argc, char **argv) {
     argc--, argv++;
-    if (argc > 0)
+    
+    
+    if (argc == 0) {
+        char repl[1024] = "                                 \
+        (fun repl-help () {                                 \
+            (println \"SHIT\")                              \
+        })                                                  \
+        (fun repl () {                                      \
+            (setl command (conti 1))                        \
+            (while 1 {                                      \
+                (print \"neta -> \")                        \
+                (-> (command (readln)))                     \
+                (match command                              \
+                    \":help\" (repl-help)                   \
+                    \":quit\" (return-f @nil)               \
+                    @default (println (eval command)))      \
+            })                                              \
+        })                                                  \
+        (repl)";
+        FILE *tmp = fopen("/tmp/repl.neta", "w");
+        fwrite(repl, sizeof(char), strlen(repl), tmp);
+        fclose(tmp);
+        yyin = fopen("/tmp/repl.neta", "r");
+    } else {
         yyin = fopen(argv[0], "r");
-    else
-        yyin = stdin;
+    }
     yylex();
     init();
     // print_parse_tree();
